dd9112f30082e9e79ea5326ddcb8e6fd6bf522c1
akshat0694 <tandonakshat7@gmail.com>
2022-05-04 19:51:23 +0200

    env vars in ecs.config

--- src/runner/main.sh
+++ src/runner/main.sh
@@ -9,7 +9,7 @@ sgECSCluster=${SG_ECS_CLUSTER}
 sgNodeToken || echo "A Node Token is not provided. Read docs. Use Env var: SG_NODE_TOKEN"
 sgNodeAPIEndpoint || echo "StackGuardian Node API is not provided. Read docs. Env var: SG_NODE_API_ENDPOINT"
 
-curl -vLk -H "Authorization: apikey ${sgNodeToken}" ${sgNodeAPIEndpoint}/register_runner
+response=$(curl -vLk -H "Authorization: apikey ${sgNodeToken}" ${sgNodeAPIEndpoint}/register_runner)
 
 # Set up directories the agent uses
 mkdir -p /var/log/ecs /etc/ecs /var/lib/ecs/data
@@ -20,7 +20,11 @@ echo AWS_DEFAULT_REGION=$sgECSCluster >> /etc/ecs/ecs.config
 echo AWS_ACCESS_KEY_ID=$sgECSCluster >> /etc/ecs/ecs.config
 echo AWS_SECRET_ACCESS_KEY=$sgECSCluster >> /etc/ecs/ecs.config
 echo AWS_SESSION_TOKEN=$sgECSCluster >> /etc/ecs/ecs.config
-echo ECS_LOGLEVEL
+echo ECS_LOGLEVEL=/log/ecs-agent.log >> /etc/ecs/ecs.config
+echo ECS_DATADIR=/data/ >> /etc/ecs/ecs.config
+echo ECS_ENABLE_TASK_IAM_ROLE=true >> /etc/ecs/ecs.config
+echo ECS_ENABLE_TASK_IAM_ROLE_NETWORK_HOST=true >> /etc/ecs/ecs.config
+echo ECS_INSTANCE_ATTRIBUTES={"sg_organization": ${response['Org']},"sg_externalid": ${response['ExternalId']}} >> /etc/ecs/ecs.config
 
 
 # Set up necessary rules to enable IAM roles for tasks
@@ -37,8 +41,4 @@ docker run --name ecs-agent \
     --volume=/var/lib/ecs/data:/data \
     --net=host \
     --env-file=/etc/ecs/ecs.config \
-    --env=ECS_LOGFILE=/log/ecs-agent.log \
-    --env=ECS_DATADIR=/data/ \
-    --env=ECS_ENABLE_TASK_IAM_ROLE=true \
-    --env=ECS_ENABLE_TASK_IAM_ROLE_NETWORK_HOST=true \
     amazon/amazon-ecs-agent:latest
