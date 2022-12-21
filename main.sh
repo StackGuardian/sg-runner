08575c9f667c7be9f85a4f4de55a17d97a2aa577
hllvc <hllvc@icloud.com>
2022-12-21 23:46:01 +0100

    refactor most of script, pre-final version

--- main.sh
+++ main.sh
@@ -1,53 +1,80 @@
-#!/usr/bin/env bash
+#!/bin/bash
+#
+# Register external instance to AWS ECS with ecs-anywhere script.
 
-# set -e -o "pipefail"
+set -o pipefail
 
 ## colors for printf
-C_RED_BOLD="\033[1;31m"
-C_RED="\033[0;31m"
-C_GREEN_BOLD="\033[1;32m"
-C_GREEN="\033[0;32m"
-C_YELLOW_BOLD="\033[1;33m"
-C_YELLOW="\033[0;33m"
-C_BLUE_BOLD="\033[1;34m"
-C_BLUE="\033[0;34m"
-C_MAGENTA_BOLD="\033[1;35m"
-C_MAGENTA="\033[0;35m"
-C_CYAN_BOLD="\033[1;36m"
-C_CYAN="\033[0;36m"
-C_RESET="\033[0m"
-C_BOLD="\033[1m"
+readonly C_RED_BOLD="\033[1;31m"
+readonly C_RED="\033[0;31m"
+readonly C_GREEN_BOLD="\033[1;32m"
+readonly C_GREEN="\033[0;32m"
+readonly C_YELLOW_BOLD="\033[1;33m"
+readonly C_YELLOW="\033[0;33m"
+readonly C_BLUE_BOLD="\033[1;34m"
+readonly C_BLUE="\033[0;34m"
+readonly C_MAGENTA_BOLD="\033[1;35m"
+readonly C_MAGENTA="\033[0;35m"
+readonly C_CYAN_BOLD="\033[1;36m"
+readonly C_CYAN="\033[0;36m"
+readonly C_RESET="\033[0m"
+readonly C_BOLD="\033[1m"
 
-_err() {
-  printf "\n${C_RED_BOLD}==> [ERROR] ${C_RED}%s" "$*" 1>&2
-  exit 1
+## API response values
+ECS_CLUSTER=
+AWS_DEFAULT_REGION=
+SSM_ACTIVATION_ID=
+SSM_ACTIVATION_CODE=
+ORGANIZATION_NAME=
+ORGANIZATION_ID=
+EXTERNAL_ID=
+RESOURCE_ID=
+
+log_date() {
+  printf "${C_MAGENTA}[%s]" "$(date +'%Y-%m-%dT%H:%M:%S%z')"
 }
 
-_help() {
-  echo ""
-  echo "$(basename "$0") [register deregister]"
+err() {
+  printf "\n%s ${C_RED_BOLD}[ERROR] ${C_RESET}%s${C_BOLD} %s${C_RESET} %s" "$(log_date)" "${1}" "${2}" "${@:3}" >&2
+  printf "\n\n(Try ${C_BOLD}%s --help${C_RESET} for more information.)" "$(basename "${0}")"
+}
+
+show_help() {
+  printf "\n${C_BOLD}%s${C_RESET} [register deregister] arguments..\n" "$(basename "${0}")"
+  printf "\n  Required arguments:\n"
+  printf "\t--sg-node-token\t\tToken provided by StackGuardian platform.\n"
+  printf "\t--sg-node-api-endpoint\tStackGuardian API endpoint.\n"
+  printf "\n  Optional arguments:\n"
+  printf "\t--help\t\t\tShow this help menu.\n"
   exit 2
 }
 
-_check_args_value() {
-  if [[ "${2:0:2}" == "--" ]]; then
-    _err "Argument $1 has invalid value: $2."
-  elif [[ -z "${2}" ]]; then
-    _err "Argument $1 is empty."
+#######################################
+# Fetch necessary info from API.
+# Globals:
+#   SG_NODE_TOKEN
+#   SG_NODE_API_ENDPOINT
+# Arguments:
+#   None
+# Outputs:
+#   Write to STDERR if error and exit.
+#######################################
+fetch_organization_info() {
+  local response
+
+  printf "\n%s ${C_GREEN}INFO:${C_RESET} Fetching data..\n" "$(log_date)"
+  if ! response=$(curl -fSsLk -H "Authorization: apikey ${SG_NODE_TOKEN}" "${SG_NODE_API_ENDPOINT}"/register_runner); then
+    err "Could not fetch data from API"
+    exit 1
   fi
-}
 
-_fetch_credentials() {
-  response=$(curl -vLk -H "Authorization: apikey ${SG_NODE_TOKEN}" "${SG_NODE_API_ENDPOINT}"/register_runner)
-
+  # TODO(devops@hllvc.com): save response to env variables
   # response={
   #     "AWSDefaultRegion": "",
-  #     "AWSAccessKeyId": "",
-  #     "AWSSecretAccessKey": "",
-  #     "AWSSessionToken": "",
-  #     "IAMRoleName",
+  #     "ECSCluster": "",
   #     "SSMActivationId": "",
   #     "SSMActivationCode": "",
+  #     "OrgName": "",
   #     "OrgId": "",
   #     "ExternalId": "",
   #     "ResourceId": ""
@@ -55,11 +82,82 @@ _fetch_credentials() {
 
 }
 
-_register_instance() {
+#######################################
+# Configure local direcotries and files.
+# Globals:
+#   ECS_CLUSTER
+#   AWS_DEFAULT_REGION
+#   ORGANIZATION_ID
+#   EXTERNAL_ID
+#   RESOURCE_ID
+# Arguments:
+#   None
+# Outputs:
+#   Writes STOUT on success.
+#######################################
+configure_local_data() {
+  # Set up directories the agent uses
+  mkdir -p /var/log/ecs /etc/ecs /var/lib/ecs/data
+  rm -rf /etc/ecs/ecs.config /var/lib/ecs/ecs.config > /dev/null
+  # rm -rf /var/lib/amazon/ssm/Vault/Store/*
 
-  # curl --proto "https" -o "/tmp/ecs-anywhere-install.sh" "https://amazon-ecs-agent.s3.amazonaws.com/ecs-anywhere-install-latest.sh"
+  cat > /etc/ecs/ecs.config << EOF
+  ECS_CLUSTER=${SG_ECS_CLUSTER}
+  AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION}
+  ECS_INSTANCE_ATTRIBUTES={"sg_organization": "${ORGANIZATION_NAME}","sg_externalid": "${EXTERNAL_ID}"}
+  ECS_LOGLEVEL=/log/ecs-agent.log
+  ECS_DATADIR=/data/
+  ECS_ENABLE_TASK_IAM_ROLE=true
+  ECS_ENABLE_TASK_IAM_ROLE_NETWORK_HOST=true
+EOF
 
-  # TODO: verify ecs-anywhere script integrity
+  cat > /var/lib/ecs/ecs.config << EOF
+  AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION}
+  ECS_EXTERNAL=true
+EOF
+
+  printf "\n%s ${C_GREEN_BOLD}INFO: ${C_GREEN}Configured local data." "$(log_date)"
+}
+
+#######################################
+# Configure local network.
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   Writes STOUT on success.
+#######################################
+configure_local_network() {
+  # Set up necessary rules to enable IAM roles for tasks
+  sysctl -w net.ipv4.conf.all.route_localnet=1
+  # sysctl -w net.ipv4.ip_forward=1
+  iptables -t nat -A PREROUTING -p tcp -d 169.254.170.2 --dport 80 -j DNAT --to-destination 127.0.0.1:51679
+  iptables -t nat -A OUTPUT -d 169.254.170.2 -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 51679
+
+  printf "\n%s ${C_GREEN_BOLD}INFO: ${C_GREEN}Configured local network." "$(log_date)"
+}
+
+#######################################
+# Register instance to AWS ECS.
+# Globals:
+#   AWS_DEFAULT_REGION
+#   ECS_CLUSTER
+#   SSM_ACTIVATION_ID
+#   SSM_ACTIVATION_CODE
+# Arguments:
+#   None
+# Outputs:
+#   Write to STDOUT/STERR
+#   if successfull/error.
+#######################################
+register_instance() {
+  if ! curl -fSsL --proto "https" -o "/tmp/ecs-anywhere-install.sh" "https://amazon-ecs-agent.s3.amazonaws.com/ecs-anywhere-install-latest.sh"; then
+    err "Unable to download" "ecs-anywhere-install.sh" "script"
+    exit 1
+  fi
+
+  # TODO(devops@hllvc.com): verify ecs-anywhere script integrity
   # gpg --keyserver hkp://keys.gnupg.net:80 --recv BCE9D9A42D51784F
   # curl --proto "https" -o "/tmp/ecs-anywhere-install.sh.asc" "https://amazon-ecs-agent.s3.amazonaws.com/ecs-anywhere-install-latest.sh.asc"
   # gpg --verify /tmp/ecs-anywhere-install.sh.asc /tmp/ecs-anywhere-install.sh
@@ -73,122 +171,128 @@ _register_instance() {
   # Primary key fingerprint: F34C 3DDA E729 26B0 79BE  AEC6 BCE9 D9A4 2D51 784F
   #      Subkey fingerprint: D64B B6F9 0CF3 77E9 B5FB  346F 50DE CCC4 710E 61AF
 
-  /usr/bin/bash /tmp/ecs-anywhere-install.sh \
+  if ! /bin/bash /tmp/ecs-anywhere-install.sh \
       --region "${AWS_DEFAULT_REGION}" \
-      --cluster "${SG_ECS_CLUSTER}" \
+      --cluster "${ECS_CLUSTER}" \
       --activation-id "${SSM_ACTIVATION_ID}" \
-      --activation-code "${SSM_ACTIVATION_CODE}"
+      --activation-code "${SSM_ACTIVATION_CODE}"; then
+    err "Script" "ecs-anywhere-install.sh" "failed to register external instance"
+    exit 1
+  fi
 
-  source ecs-anywhere-install.sh --region ${AWS_DEFAULT_REGION} --cluster ${ECS_CLUSTER} --activation-id "${SSM_ACTIVATION_ID}" --activation-code "${SSM_ACTIVATION_CODE}"
+  printf "\n%s ${C_GREEN_BOLD}INFO: ${C_GREEN}Instance successfully registered to ECS cluster." "$(log_date)"
 }
 
-_deregister_instance() {
-  aws ecs deregister-container-instance --cluster ${ECS_CLUSTER} --container-instance ${}
+#######################################
+# Make API call for de-registering.
+# Globals:
+#   SG_NODE_API_ENDPOINT
+#   SG_NODE_TOKEN
+# Arguments:
+#   None
+# Outputs:
+#   Writes to STDOUT/STDERR
+#   if de-registration is sucessfull.
+#######################################
+deregister_instance() {
+  ## TODO(devops@hllvc.com): Handle de-registration process.
+  return 0
+  response=$(curl -vLk -H "Authorization: apikey ${SG_NODE_TOKEN}" "${SG_NODE_API_ENDPOINT}"/deregister_runner)
 }
 
-if [[ $(id -u) -ne 0 ]]; then
-  _err "This script must be run as root."
-fi
+#######################################
+# Check if provided argument is valid.
+# Globals:
+#   None
+# Arguments:
+#   Argument name
+#   Argument value
+# Returns:
+#   0 if argument is valid.
+# Outputs:
+#   If error, write to STDERR and exit.
+#######################################
+check_arg_value() {
+  if [[ "${2:0:2}" == "--" ]]; then
+    err "Argument" "${1}" "has invalid value: $2"
+    exit 1
+  elif [[ -z "${2}" ]]; then
+    err "Argument" "${1}" "can't be empty"
+    exit 1
+  fi
+  return 0
+}
 
-if [[ "$#" -lt 1 || ! "$*" =~ register|deregister ]]; then
-  _help
-fi
+is_root() {
+  if (( $(id -u) != 0 )); then
+    err "This script must be run as" "root"
+    exit 1
+  fi
+  return 0
+}
 
-OPTION=$1
+init_args_are_valid() {
+  if (( $# != 5 )); then
+    err "Invalid count of arguments passed"
+  elif [[ ! "$1" =~ register|deregister ]]; then
+    err "Provided option" "${1}" "is invalid"
+  fi
+  return 0
+}
+
+check_sg_args() {
+  if [[ -z "$SG_NODE_TOKEN" && -z "$SG_NODE_API_ENDPOINT" && -z "$SG_ECS_CLUSTER" ]]; then
+    err "Arguments: " "--sg-node-token, --sg-node-api-endpoint, --sg-ecs-cluster" "are required"
+    exit 1
+  fi
+  return 0
+}
+
+main() {
+
+[[ "${*}" =~ "--help" ]] && show_help && exit 0
+
+is_root && init_args_are_valid "$@"
+
+OPTION="${1}"
 shift
 
 while :; do
-  case "$1" in
+  case "${1}" in
   --sg-node-token)
-    _check_args_value "$1" "$2"
-    SG_NODE_TOKEN="$2"
+    check_arg_value "${1}" "${2}"
+    SG_NODE_TOKEN="${2}"
     shift 2
     ;;
   --sg-node-api-endpoint)
-    _check_args_value "$1" "$2"
-    SG_NODE_API_ENDPOINT="$2"
-    shift 2
-    ;;
-  --sg-ecs-cluster)
-    _check_args_value "$1" "$2"
-    SG_ECS_CLUSTER="$2"
+    check_arg_value "${1}" "${2}"
+    SG_NODE_API_ENDPOINT="${2}"
     shift 2
     ;;
   *)
-    [ -z "$1" ] && break
-    _err "Invalid option: [$1]"
+    [[ -z "${1}" ]] && break
+    err "Invalid argument:" "${1}"
+    exit 1
     ;;
   esac
 done
+readonly SG_NODE_TOKEN SG_NODE_API_ENDPOINT
 
-if [[ -z "$SG_NODE_TOKEN" && -z "$SG_NODE_API_ENDPOINT" && -z "$SG_ECS_CLUSTER" ]]; then
-  _err "Arguments: --sg-node-token, --sg-node-api-endpoint, --sg-ecs-cluster are required."
-fi
+check_sg_args
+#fetch_organization_info
+#configure_local_data
+#configure_local_network
 
-echo "SG_NODE_TOKEN: ${SG_NODE_TOKEN}, SG_NODE_API_ENDPOINT: ${SG_NODE_API_ENDPOINT}, SG_ECS_CLUSTER: ${SG_ECS_CLUSTER}, OPTION: ${OPTION}" && exit 0
-
-# TODO: save responses to variables for further usage in script
-SG_ECS_CLUSTER=private-runner-test
-#
-AWS_DEFAULT_REGION=eu-central-1
-AWS_ACCESS_KEY_ID=
-AWS_SECRET_ACCESS_KEY=
-AWS_SESSION_TOKEN=
-IAM_ROLE_NAME=
-SSM_ACTIVATION_ID=
-SSM_ACTIVATION_CODE=
-ORGANIZATION_ID=
-EXTERNAL_ID=
-RESOURCE_ID=
-
-# Set up directories the agent uses
-mkdir -p /var/log/ecs /etc/ecs /var/lib/ecs/data
-rm -rf /etc/ecs/ecs.config /var/lib/ecs/ecs.config
-# rm -rf /var/lib/amazon/ssm/Vault/Store/*
-
-# echo ECS_CLUSTER=$sgECSCluster >> /etc/ecs/ecs.config
-# echo AWS_DEFAULT_REGION=${response['AWSDefaultRegion']} >> /etc/ecs/ecs.config
-# echo AWS_ACCESS_KEY_ID=${response['AWSAccessKeyId']} >> /etc/ecs/ecs.config
-# echo AWS_SECRET_ACCESS_KEY=${response['AWSSecretAccessKey']} >> /etc/ecs/ecs.config
-# echo AWS_SESSION_TOKEN=${response['AWSSessionToken']} >> /etc/ecs/ecs.config
-# echo ECS_INSTANCE_ATTRIBUTES={"sg_organization": ${response['OrgName']},"sg_externalid": ${response['ExternalId']}} >> /etc/ecs/ecs.config
-
-# echo ECS_LOGLEVEL=/log/ecs-agent.log >> /etc/ecs/ecs.config
-# echo ECS_DATADIR=/data/ >> /etc/ecs/ecs.config
-# echo ECS_ENABLE_TASK_IAM_ROLE=true >> /etc/ecs/ecs.config
-# echo ECS_ENABLE_TASK_IAM_ROLE_NETWORK_HOST=true >> /etc/ecs/ecs.config
-
-cat > /etc/ecs/ecs.config << EOF
-ECS_CLUSTER=${SG_ECS_CLUSTER}
-AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION}
-ECS_INSTANCE_ATTRIBUTES={"sg_organization": "test","sg_externalid": "1"}
-ECS_LOGLEVEL=/log/ecs-agent.log
-ECS_DATADIR=/data/
-ECS_ENABLE_TASK_IAM_ROLE=true
-ECS_ENABLE_TASK_IAM_ROLE_NETWORK_HOST=true
-EOF
-
-cat > /var/lib/ecs/ecs.config << EOF
-AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION}
-ECS_EXTERNAL=true
-EOF
-
-# Set up necessary rules to enable IAM roles for tasks
-sysctl -w net.ipv4.conf.all.route_localnet=1
-# sysctl -w net.ipv4.ip_forward=1
-iptables -t nat -A PREROUTING -p tcp -d 169.254.170.2 --dport 80 -j DNAT --to-destination 127.0.0.1:51679
-iptables -t nat -A OUTPUT -d 169.254.170.2 -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 51679
-
-case "$OPTION" in
+case "${OPTION}" in
   register)
-    _register_instance
-    exit 0
+    register_instance
     ;;
   deregister)
-    _deregister_instance
+    deregister_instance
     ;;
 esac
 
+exit 0
 # Run the agent
 # TODO: check which volumes will be exported?
 # docker run --name ecs-agent9 \
@@ -202,3 +306,7 @@ esac
 #     amazon/amazon-ecs-agent:latest
 
 # TODO: API call to ping the node
+#
+}
+
+main "$@"
