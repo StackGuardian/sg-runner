fba6b36602ad524a37b378b38cde75fb7f94b40b
Adis Halilovic <adis.haliovic@stackguardian.io>
2023-04-20 23:36:28 +0200

    tweak spinner, fail-restart, api call, logs..

--- main.sh
+++ main.sh
@@ -9,19 +9,19 @@ readonly C_RED_BOLD="\033[1;31m"
 readonly C_RED="\033[0;31m"
 readonly C_GREEN_BOLD="\033[1;32m"
 readonly C_GREEN="\033[0;32m"
-readonly C_YELLOW_BOLD="\033[1;33m"
-readonly C_YELLOW="\033[0;33m"
-readonly C_BLUE_BOLD="\033[1;34m"
+# readonly C_YELLOW_BOLD="\033[1;33m"
+# readonly C_YELLOW="\033[0;33m"
+# readonly C_BLUE_BOLD="\033[1;34m"
 readonly C_BLUE="\033[0;34m"
 readonly C_MAGENTA_BOLD="\033[1;35m"
-readonly C_MAGENTA="\033[0;35m"
-readonly C_CYAN_BOLD="\033[1;36m"
-readonly C_CYAN="\033[0;36m"
+# readonly C_MAGENTA="\033[0;35m"
+# readonly C_CYAN_BOLD="\033[1;36m"
+# readonly C_CYAN="\033[0;36m"
 readonly C_RESET="\033[0m"
 readonly C_BOLD="\033[1m"
 
 log_date() {
-  printf "${C_MAGENTA}[%s]" "$(date +'%Y-%m-%dT%H:%M:%S%z')"
+  printf "${C_BLUE}[%s]" "$(date +'%Y-%m-%dT%H:%M:%S%z')"
 }
 
 err() {
@@ -31,14 +31,19 @@ err() {
 }
 
 info() {
-  printf "%s ${C_GREEN}INFO:${C_RESET} %s${C_BOLD} %s${C_RESET} %s\n" "$(log_date)" "${1}" "${2}" "${@:3}"
+  printf "%s ${C_GREEN_BOLD}[INFO]${C_RESET} %s${C_BOLD} %s${C_RESET} %s\n" "$(log_date)" "${1}" "${2}" "${@:3}"
+}
+
+debug() {
+  printf "%s ${C_MAGENTA_BOLD}[DEBUG]${C_RESET} %s${C_BOLD} %s${C_RESET} %s\n" "$(log_date)" "${1}" "${2}" "${@:3}"
 }
 
 show_help() {
   printf "\nsudo ${C_BOLD}%s${C_RESET} [register deregister] arguments..\n" "$(basename "${0}")"
   printf "\n  Required arguments:\n"
   printf "\t--sg-node-token\t\tToken provided by StackGuardian platform.\n"
-  printf "\t--sg-node-api-endpoint\tStackGuardian API endpoint.\n"
+  printf "\t--organization\tOrganization on Stackguardian with Runner Group.\n"
+  printf "\t--runner-group\tRunner Group inside organization.\n"
   printf "\n  Optional arguments:\n"
   printf "\t--debug\t\t\tShow log output.\n"
   printf "\t--help\t\t\tShow this help menu.\n"
@@ -46,11 +51,12 @@ show_help() {
 }
 
 spinner() {
-    local pid=$1
+    spinner_pid=$1
+    local log_file="$2"
     local delay=0.15
     local spinstr='|/-\'
     if [[ "${LOG_DEBUG}" == "false" ]]; then
-      while ps a | awk '{print $1}' | grep "${pid}" >&/dev/null; do
+      while ps a | awk '{print $1}' | grep "${spinner_pid}" >&/dev/null; do
           local temp=${spinstr#?}
           printf " [%c] " "$spinstr"
           local spinstr=$temp${spinstr%"$temp"}
@@ -58,26 +64,20 @@ spinner() {
           printf "\b\b\b\b\b\b"
       done
     else
-      tail -n0 -f /var/log/ecs-install.log --pid "${pid}"
+      tail -n0 -f "${log_file}" --pid "${spinner_pid}"
     fi
-    wait "${pid}"
+    wait "${spinner_pid}"
     local exit_status=$?
-    if (( "${exit_status}" != 0 )); then
-      # err "Process failed with exit code status" "${exit_status}"
-      if [[ "${LOG_DEBUG}" == "true" ]]; then
-        info "Showing 10 last lines from" "/var/log/ecs/ecs-init.log"
-        tail -n10 /var/log/ecs/ecs-init.log
-      fi
-      err "Script" "ecs-anywhere-install.sh" "failed to register external instance"
-    fi
     printf "    \b\b\b\b"
+    return $exit_status
 }
 
 #######################################
 # Fetch necessary info from API.
 # Globals:
 #   SG_NODE_TOKEN
-#   SG_NODE_API_ENDPOINT
+#   ORGANIZATION_ID
+#   RUNNER_GROUP_ID
 # Arguments:
 #   None
 # Outputs:
@@ -87,14 +87,28 @@ spinner() {
 fetch_organization_info() {
   local response
   local metadata
+  local url
 
-  # info "Fetching data..."
-  # if ! response=$(curl -fSsLk -H "Authorization: apikey ${SG_NODE_TOKEN}" "${SG_NODE_API_ENDPOINT}/orgs/${ORGANIZATION_ID}/runnergroups/${RESOURCE_ID}/register/"); then
-  #   err "Could not fetch data from API"
-  #   exit 1
-  # fi
+  . .env
 
-  response=$(cat data.json)
+  info "Trying to fetch registration data.."
+
+  url="${SG_NODE_API_ENDPOINT}/orgs/${ORGANIZATION_ID}/runnergroups/${RUNNER_GROUP_ID}/register/"
+
+  [[ ${LOG_DEBUG} == "true" ]] && debug "Calling URL:" "${url}"
+
+  if ! response=$(curl -fSsLk \
+    -X POST \
+    -H "Authorization: apikey ${SG_NODE_TOKEN}" \
+    -H "Content-Type: application/json" \
+    "${url}"); then
+    # printf "Response: %s" "${response}" | jq
+    err "Could not fetch data from API."
+  else
+    info "Registration data fetched. Preparing environment.."
+  fi
+
+  # response=$(cat data.json)
 
   ## API response values (Registration Metadata)
   metadata="$(echo "${response}" | jq -r '.data.RegistrationMetadata[0]')"
@@ -102,30 +116,34 @@ fetch_organization_info() {
   AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION:=$(echo "${metadata}" | jq -r '.AWSDefaultRegion')}"
   SSM_ACTIVATION_ID="${SSM_ACTIVATION_ID:=$(echo "${metadata}" | jq -r '.SSMActivationId')}"
   SSM_ACTIVATION_CODE="${SSM_ACTIVATION_CODE:=$(echo "${metadata}" | jq -r '.SSMActivationCode')}"
+  AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID:=$(echo "${metadata}" | jq -r '.AWSAccessKeyId')}"
+  AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY:=$(echo "${metadata}" | jq -r '.AWSSecretAccessKey')}"
 
   if [[ "${LOG_DEBUG}" == "true" ]]; then
-    echo "ECS_CLUSTER: ${ECS_CLUSTER}"
-    echo "AWS_DEFAULT_REGION: ${AWS_DEFAULT_REGION}"
-    echo "SSMActivationId: ${SSM_ACTIVATION_ID}"
-    echo "SSM_ACTIVATION_CODE: ${SSM_ACTIVATION_CODE}"
+    debug "ECS_CLUSTER:" "${ECS_CLUSTER}"
+    debug "AWS_DEFAULT_REGION:" "${AWS_DEFAULT_REGION}"
+    debug "SSM_ACTIVATION_ID:" "${SSM_ACTIVATION_ID:0:5}*****"
+    debug "SSM_ACTIVATION_CODE:" "${SSM_ACTIVATION_CODE:0:5}*****"
+    debug "AWS_ACCESS_KEY_ID:" "${AWS_ACCESS_KEY_ID:0:5}*****"
+    debug "AWS_SECRET_ACCESS_KEY:" "${AWS_SECRET_ACCESS_KEY:0:5}*****"
   fi
 
   ## Everything else
   ORGANIZATION_NAME="${ORGANIZATION_NAME:=$(echo "${response}" | jq -r '.data.OrgName')}"
   ORGANIZATION_ID="${ORGANIZATION_ID:=$(echo "${response}" | jq -r '.data.OrgId')}"
-  EXTERNAL_ID="${EXTERNAL_ID:=$(echo "${response}" | jq -r '.data.RunnerId')}"
-  RESOURCE_ID="${RESOURCE_ID:=$(echo "${response}" | jq -r '.data.RunnerGroupId')}"
-  AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID:=$(echo "${response}" | jq -r '.data.AWSAccessKeyId')}"
-  AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY:=$(echo "${response}" | jq -r '.data.AWSSecretAccessKey')}"
+  RUNNER_ID="${RUNNER_ID:=$(echo "${response}" | jq -r '.data.RunnerId')}"
+  RUNNER_GROUP_ID="${RUNNER_GROUP_ID:=$(echo "${response}" | jq -r '.data.RunnerGroupId')}"
+  TAGS="${TAGS:=$(echo "${response}" | jq -r '.data.tags')}"
 
   if [[ "${LOG_DEBUG}" == "true" ]]; then
-    echo "ORGANIZATION_NAME: ${ORGANIZATION_NAME}"
-    echo "ORGANIZATION_ID: ${ORGANIZATION_ID}"
-    echo "EXTERNAL_ID: ${EXTERNAL_ID}"
-    echo "RESOURCE_ID: ${RESOURCE_ID}"
-    echo "AWS_ACCESS_KEY_ID: ${AWS_ACCESS_KEY_ID}"
-    echo "AWS_SECRET_ACCESS_KEY: ${AWS_SECRET_ACCESS_KEY}"
+    debug "ORGANIZATION_NAME:" "${ORGANIZATION_NAME}"
+    debug "ORGANIZATION_ID:" "${ORGANIZATION_ID}"
+    debug "RUNNER_ID:" "${RUNNER_ID}"
+    debug "RUNNER_GROUP_ID:" "${RUNNER_GROUP_ID}"
+    debug "TAGS:" "${TAGS}"
   fi
+
+  info "Environment ready."
 
 }
 
@@ -135,8 +153,8 @@ fetch_organization_info() {
 #   ECS_CLUSTER
 #   AWS_DEFAULT_REGION
 #   ORGANIZATION_ID
-#   EXTERNAL_ID
-#   RESOURCE_ID
+#   RUNNER_ID
+#   RUNNER_GROUP_ID
 # Arguments:
 #   None
 # Outputs:
@@ -148,11 +166,12 @@ configure_local_data() {
   rm -rf /etc/ecs/ecs.config /var/lib/ecs/ecs.config > /dev/null
   # rm -rf /var/lib/amazon/ssm/Vault/Store/*
 
-  cat > /etc/ecs/ecs.config << EOF
   info "Configuring local data.."
+
+  cat > /etc/ecs/ecs.config << EOF
 ECS_CLUSTER=${ECS_CLUSTER}
 AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION}
-ECS_INSTANCE_ATTRIBUTES={"sg_organization": "${ORGANIZATION_NAME}","sg_externalid": "${EXTERNAL_ID}"}
+ECS_INSTANCE_ATTRIBUTES={"sg_organization": "${ORGANIZATION_NAME}","sg_runner_id": "${RUNNER_ID}", "sg_runner_group_id": "${RUNNER_GROUP_ID}", "tags": ${TAGS}}
 ECS_LOGLEVEL=/log/ecs-agent.log
 ECS_DATADIR=/data/
 ECS_ENABLE_TASK_IAM_ROLE=true
@@ -163,6 +182,8 @@ EOF
 AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION}
 ECS_EXTERNAL=true
 EOF
+
+  info "Local data conifgured."
 
 }
 
@@ -190,6 +211,8 @@ configure_local_network() {
   iptables -t nat -A PREROUTING -p tcp -d 169.254.170.2 --dport 80 -j DNAT --to-destination 127.0.0.1:51679
   iptables -t nat -A OUTPUT -d 169.254.170.2 -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 51679
 
+  info "Local network configured."
+
 }
 
 #######################################
@@ -205,6 +228,7 @@ configure_local_network() {
 #######################################
 configure_fluentbit() {
   info "Starting fluentbit agent.."
+
   local running=$(docker ps -q --filter "name=fluentbit-agent")
   local exists=$(docker ps -aq --filter "name=fluentbit-agent")
   if [[ -z "${exists}" ]]; then
@@ -245,6 +269,7 @@ check_systemctl_status() {
   if ! systemctl is-active "$1" >&/dev/null; then
     info "Reloading/Restarting service.$1.."
     systemctl reload-or-restart "$1"
+    info "Done. Continuing registration.."
   fi
 }
 
@@ -303,7 +328,12 @@ check_systemcl_docker_status() {
 #   if successfull/error.
 #######################################
 register_instance() {
-  local registered=$(docker ps -q --filter "name=ecs-agent")
+  local registered
+
+  . .env
+
+  registered=$(docker ps -q --filter "name=ecs-agent")
+
   if [[ -n "${registered}" ]]; then
     info "ECS Agent already registered and running."
     configure_local_network
@@ -314,16 +344,34 @@ register_instance() {
   fetch_organization_info
   configure_local_data
 
-  rm /tmp/ecs* >&/dev/null
+  ## It is not neccessary to remove script each time
+  # rm /tmp/ecs* >&/dev/null
 
-  curl -fSsL --proto "https" -o "/tmp/ecs-anywhere-install.sh" "https://amazon-ecs-agent.s3.amazonaws.com/ecs-anywhere-install-latest.sh" 2> /tmp/ecs_anywhere_download.log
+  if [[ ! -e /tmp/ecs-anywhere-install.sh ]]; then
+    info "Downloading ecs-anywhere-install.sh.."
 
-  if (( $? != 0 )); then
-    cat /tmp/ecs_anywhere_download.log
-    err "Unable to download" "ecs-anywhere-install.sh" "script"
+    if ! curl -fSsLk \
+      --proto "https" \
+      -o "/tmp/ecs-anywhere-install.sh" \
+      "https://amazon-ecs-agent.s3.amazonaws.com/ecs-anywhere-install-latest.sh" \
+      >&/tmp/ecs_anywhere_download.log; then
+      [[ "${LOG_DEBUG}" == "true" ]] \
+        && debug "Response:" "$(cat /tmp/ecs_anywhere_download.log)"
+      err "Unable to download" "ecs-anywhere-install.sh" "script"
+    else
+      info "Download completed. Continuing.."
+    fi
+  else
+    info "Script ecs-anywhere-install.sh already exists."
   fi
 
-  # TODO(devops@hllvc.com): verify ecs-anywhere script integrity
+  ## old code
+  # if (( $? != 0 )); then
+  #   cat /tmp/ecs_anywhere_download.log
+  #   err "Unable to download" "ecs-anywhere-install.sh" "script"
+  # fi
+
+  # TODO(adis.halilovic@stackguardian.io): verify ecs-anywhere script integrity
   # gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv BCE9D9A42D51784F
   # curl --proto "https" -o "/tmp/ecs-anywhere-install.sh.asc" "https://amazon-ecs-agent.s3.amazonaws.com/ecs-anywhere-install-latest.sh.asc"
   # gpg --verify /tmp/ecs-anywhere-install.sh.asc /tmp/ecs-anywhere-install.sh | grep "Primary key"
@@ -331,40 +379,25 @@ register_instance() {
   # Primary key fingerprint: F34C 3DDA E729 26B0 79BE  AEC6 BCE9 D9A4 2D51 784F
   #      Subkey fingerprint: D64B B6F9 0CF3 77E9 B5FB  346F 50DE CCC4 710E 61AF
 
-  info "Trying to register instance to ECS cluster..."
+  info "Trying to register instance to ECS cluster.."
 
   check_systemcl_docker_status
   check_systemcl_ecs_status
 
-  [[ ! -e /var/log/ecs-install.log ]] && touch /var/log/ecs-install.log
+  [[ ! -e /var/log/ecs-install.log ]] \
+    && touch /var/log/ecs-install.log
 
-  # if [[ "${LOG_DEBUG}" == "true" ]]; then
-  #   tail -q -f /var/log/ecs-install.log &
-  #   tail_pid="$!"
-  # fi
-
-  /bin/bash /tmp/ecs-anywhere-install.sh \
+  if ! /bin/bash /tmp/ecs-anywhere-install.sh \
       --region "${AWS_DEFAULT_REGION}" \
       --cluster "${ECS_CLUSTER}" \
       --activation-id "${SSM_ACTIVATION_ID}" \
       --activation-code "${SSM_ACTIVATION_CODE}" \
       >>/var/log/ecs-install.log 2>&1 &
-  spinner "$!"
-
-  # if ! /bin/bash /tmp/ecs-anywhere-install.sh \
-  #     --region "${AWS_DEFAULT_REGION}" \
-  #     --cluster "${ECS_CLUSTER}" \
-  #     --activation-id "${SSM_ACTIVATION_ID}" \
-  #     --activation-code "${SSM_ACTIVATION_CODE}" \
-  #     >>/var/log/ecs-install.log 2>&1 & spinner $!; then
-  #   err "Script" "ecs-anywhere-install.sh" "failed to register external instance"
-  #   [[ "${LOG_DEBUG}" == "true" ]] && tail -n10 /var/log/ecs/ecs-init.log
-  #   exit 1
-  # fi
-
-  # [[ -n ${tail_pid} ]] && kill "${tail_pid}"
-
-  info "Instance successfully registered to ECS cluster."
+  spinner "$!" "/var/log/ecs-install.log"; then
+    err "Script" "ecs-anywhere-install.sh" "failed to register external instance."
+  else
+    info "Instance successfully registered to ECS cluster."
+  fi
 
   configure_local_network
   configure_fluentbit
@@ -382,6 +415,41 @@ register_instance() {
 #   if de-registration is sucessfull.
 #######################################
 deregister_instance() {
+  local response
+  local url
+
+  . .env
+
+  if [[ -e /etc/ecs/ecs.config ]]; then
+    RUNNER_ID="$(grep ECS_INSTANCE_ATTRIBUTES /etc/ecs/ecs.config \
+      | cut -d "=" -f2 \
+      | jq -r '.sg_runner_id')"
+  else
+    err "Local data could not be found:" "/etc/ecs/ecs.config"
+  fi
+
+  url="${SG_NODE_API_ENDPOINT}/orgs/${ORGANIZATION_ID}/runnergroups/${RUNNER_GROUP_ID}/deregister/"
+
+  [[ ${LOG_DEBUG} == "true" ]] && debug "Calling URL:" "${url}"
+
+  payload="{ \"RunnerId\": \"${RUNNER_ID}\" }"
+
+  [[ ${LOG_DEBUG} == "true" ]] && debug "Payload:" "${payload}"
+
+  # if ! reponse=$(curl -fSsLk \
+  #   -X POST \
+  #   -H "Authorization: apikey ${SG_NODE_TOKEN}" \
+  #   -H "Content-Type: application/json" \
+  #   "${url}" \
+  #   -d "${payload}"); then
+  #   err "Could not fetch data from API."
+  # else
+  #   echo "${reponse}"
+  #   info "Instance de-registered. Removing local data.."
+  # fi
+
+  info "Stopping service.ecs.."
+  systemctl stop ecs
   info "Stopping docker containers.."
   docker stop ecs-agent fluentbit-agent >&/dev/null
   info "Removing docker containers.."
@@ -391,11 +459,7 @@ deregister_instance() {
   info "Removing local configuration.."
   rm -rf /var/log/ecs /etc/ecs /var/lib/ecs
 
-  ## TODO(devops@hllvc.com): Handle de-registration process.
-  return 0
-  local response
-
-  response=$(curl -vLk -H "Authorization: apikey ${SG_NODE_TOKEN}" "${SG_NODE_API_ENDPOINT}"/deregister_runner)
+  info "Local data removed."
 }
 
 #######################################
@@ -431,7 +495,7 @@ doctor_frame() {
 #  of services with status
 #######################################
 doctor() {
-  info "Generating service doctor.."
+  info "Checking services health status.."
   echo
 
   local status_list=""
@@ -464,7 +528,8 @@ doctor() {
   local containers=( "ecs" "fluentbit" )
 
   for container in "${containers[@]}"; do
-    local container_status="$(docker ps \
+    local container_status
+    container_status="$(docker ps \
       --filter "name=${container}-agent" \
       --format '{{.Status}}'\
       )"
@@ -481,7 +546,7 @@ doctor() {
   doctor_frame "Container Status" "${status_list}"
 
   echo
-  info "Service doctor finished."
+  info "Services health status generated."
 }
 
 #######################################
@@ -497,13 +562,11 @@ doctor() {
 #   If error, write to STDERR and exit.
 #######################################
 check_arg_value() {
-  ## TODO(devops@hllvc.com): make sure to validate double parameter input
+  ## TODO(adis.halilovic@stackguardian.io): make sure to validate double parameter input
   if [[ "${2:0:2}" == "--" ]]; then
     err "Argument" "${1}" "has invalid value: $2"
-    exit 1
   elif [[ -z "${2}" ]]; then
     err "Argument" "${1}" "can't be empty"
-    exit 1
   fi
   return 0
 }
@@ -511,26 +574,22 @@ check_arg_value() {
 is_root() {
   if (( $(id -u) != 0 )); then
     err "This script must be run as" "root"
-    exit 1
   fi
   return 0
 }
 
 init_args_are_valid() {
-  if [[ ! "$1" =~ register|deregister ]]; then
+  if [[ ! "$1" =~ register|deregister|doctor ]]; then
     err "Provided option" "${1}" "is invalid"
-    exit 1
-  elif (( $# != 5 )); then
-    err "Arguments:" "--sg-node-token, --sg-node-api-endpoint" "are required"
-    exit 1
+  elif (( $# != 7 )); then
+    err "Arguments:" "--sg-node-token, --organization, --runner-group" "are required"
   fi
   return 0
 }
 
 check_sg_args() {
-  if [[ -z "$SG_NODE_TOKEN" && -z "$SG_NODE_API_ENDPOINT" ]]; then
-    err "Arguments: " "--sg-node-token, --sg-node-api-endpoint" "are required"
-    exit 1
+  if [[ -z "${SG_NODE_TOKEN}" && -z "${ORGANIZATION_ID}" && -z "${RUNNER_GROUP_ID}" ]]; then
+    err "Arguments: " "--sg-node-token, --organization, --runner-group" "are required"
   fi
   return 0
 }
@@ -538,11 +597,12 @@ check_sg_args() {
 main() {
 
 if ! type jq >&/dev/null; then
-  err "Command" "jq" "not installed" && exit 1
+  err "Command" "jq" "not installed"
 fi
 
 [[ "${*}" =~ "--help" || $# -lt 1 ]] && show_help && exit 0
 
+## commented for easier testing purposes
 # is_root && init_args_are_valid "$@"
 
 OPTION="${1}"
@@ -555,11 +615,6 @@ while :; do
     SG_NODE_TOKEN="${2}"
     shift 2
     ;;
-  --sg-node-api-endpoint)
-    check_arg_value "${1}" "${2}"
-    SG_NODE_API_ENDPOINT="${2}"
-    shift 2
-    ;;
   --organization)
     check_arg_value "${1}" "${2}"
     ORGANIZATION_NAME="${2}"
@@ -567,7 +622,7 @@ while :; do
     ;;
   --runner-group)
     check_arg_value "${1}" "${2}"
-    RESOURCE_ID="${2}"
+    RUNNER_GROUP_ID="${2}"
     shift 2
     ;;
   --debug)
@@ -577,12 +632,10 @@ while :; do
   *)
     [[ -z "${1}" ]] && break
     err "Invalid argument:" "${1}"
-    exit 1
     ;;
   esac
 done
-readonly SG_NODE_TOKEN SG_NODE_API_ENDPOINT
-# readonly SG_NODE_TOKEN SG_NODE_API_ENDPOINT ORGANIZATION_ID RESOURCE_ID
+
 readonly SG_DOCKER_NETWORK="wf-steps-net"
 readonly LOG_DEBUG=${LOG_DEBUG:=false}
 
@@ -598,25 +651,13 @@ case "${OPTION}" in
     ;;
 esac
 
-# Run the agent
-# TODO: check which volumes will be exported?
-# docker run --name ecs-agent9 \
-#     --detach=true \
-#     --restart=on-failure:10 \
-#     --volume=/var/run/docker.sock:/var/run/docker.sock \
-#     --volume=/var/log/ecs:/log \
-#     --volume=/var/lib/ecs/data:/data \
-#     --net=host \
-#     --env-file=/etc/ecs/ecs.config \
-#     amazon/amazon-ecs-agent:latest
-
 # TODO: API call to ping the node
-#
+
 }
 
 cleanup() {
   echo "Gracefull shutdown.."
-  [[ -n ${script_pid} ]] && kill "${script_pid}" >&/dev/null
+  [[ -n ${spinner_pid} ]] && kill "${spinner_pid}" >&/dev/null
   exit 0
 }
 
