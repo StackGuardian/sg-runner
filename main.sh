4ca40d9cf65334dbb9af672947c0a59c2cb7fada
AKSHAT TANDON <akshat.tandon@stackguardian.io>
2025-02-28 18:39:07 +0100

    Add runner group link to details output

--- main.sh
+++ main.sh
@@ -243,6 +243,7 @@ print_details() { #{{{
   # details_item "Registration Date" "$(date +'%Y-%m-%d %H:%M:%S (GMT%z)')"
   details_item "Organization" "${ORGANIZATION_ID}"
   details_item "Runner Group" "${RUNNER_GROUP_ID}"
+  details_item "Link" "https://app.stackguardian.io/orchestrator/orgs/${ORGANIZATION_NAME}/runnergroups/${RUNNER_GROUP_ID}"
   echo
   details_frame "Host Information"
   details_item "Hostaname" "$HOSTNAME"
@@ -259,9 +260,6 @@ print_details() { #{{{
   details_item "CPU Cores" "$(echo "$(nproc) Core [Used: $(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}' | awk '{printf "%.0f%%", $1}')]")"
   details_item "Memory" "$(free -h | awk '/^Mem:/ {printf "%s [Used: %s]\n", $2, $3}')"
   details_item "Size /var" "$(df -h --total /var | awk '/^total/ {if ($2 ~ /G/ && $2 + 0 < 100) printf "\033[31m%s [Used: %s]\033[0m\n", $2, $(NF-1); else printf "%s [Used: %s]\n", $2, $(NF-1)}')"
-  echo
-  details_frame "Runner Group Link"
-  details_item "Link" "https://app.stackguardian.io/orchestrator/orgs/${ORGANIZATION_NAME}/runnergroups/${RUNNER_GROUP_ID}"
   echo
 }
 #}}}: print_details
